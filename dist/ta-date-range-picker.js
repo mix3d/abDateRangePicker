2b9739d084d5fa41f64bbbe1d4290d269cfbd56d
Madison Dickson <mdickson@simbex.com>
2018-09-25 13:38:28 -0400

    Upgrade to AngularJS 1.5.11, fixes to make it actually run!
    Also cleanup excessive and incorrect != null checks for event stopPropogations

--- dist/ta-date-range-picker.js
+++ dist/ta-date-range-picker.js
@@ -1,139 +1,123 @@
 (function () {
-    angular.module("tawani.utils").directive('taDateRangePicker',
+    angular.module("tawani.utils", []).directive('taDateRangePicker',
         ["$compile", "$timeout", function ($compile, $timeout) {
-            var CUSTOM = "CUSTOM";
+            const CUSTOM = "CUSTOM";
+
+            let inputFormat = "l";
+            let pickerDateFormat = "MM/DD/YYYY";
+            let monthFormat = "MMM YYYY";
+
+            let tryStopProp = function ($event) {
+                return $event && typeof $event.stopPropagation === "function" ?
+                    $event.stopPropagation() : void 0;
+            }
 
             return {
                 scope: {
                     model: "=ngModel",
-                    ranges: "=",
+                    ranges: "=?",
                     callback: "&"
                 },
                 template: `
                 <div class="selectbox">
                     <i class="glyphicon glyphicon-calendar"></i>
-                    <span ng-show="model">{{model.start.format("LL")}}
-                        - {{model.end.format("LL")}}</span>
+                    <span ng-show="model">{{model.start.format(inputFormat)}}
+                        - {{model.end.format(inputFormat)}}</span>
                     <span ng-hide="model">Select date range</span>
                     <b class="caret"></b>
                 </div>`,
-                link: function (scope, element, attrs) {
-                    scope.weekDays = moment.weekdaysMin();
+                link: function ($scope, element, attrs) {
+                    $scope.weekDays = moment.weekdaysMin();
+
+                    $scope.inputFormat = inputFormat;
 
                     //set default ranges
-                    if (!(scope.ranges && scope.ranges.length))
-                        scope.ranges = getDefaultRanges();
+                    if (!($scope.ranges && $scope.ranges.length))
+                        $scope.ranges = getDefaultRanges();
 
-                    scope.show = function () {
+                    $scope.show = function () {
                         //clear prevs
-                        scope.currentSelection = null;
+                        $scope.currentSelection = null;
 
                         //prepare
-                        prepareMonths(scope);
-                        scope.selection = scope.model;
-                        prepareRanges(scope);
-                        return scope.visible = true;
+                        prepareMonths($scope);
+                        $scope.selection = $scope.model;
+                        prepareRanges($scope);
+                        return $scope.visible = true;
                     };
-                    scope.hide = function ($event) {
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
-                        scope.visible = false;
-                        return scope.start = null;
+                    $scope.hide = function ($event) {
+                        tryStopProp($event)
+                        $scope.visible = false;
+                        return $scope.start = null;
                     };
-                    scope.handlePickerClick = function ($event) {
-                        return $event !== null ? typeof $event.stopPropagation === "function" ? $event.stopPropagation() : void 0 : void 0;
+                    $scope.handlePickerClick = function ($event) {
+                        return tryStopProp($event);
                     };
 
-                    scope.select = function (day, $event) {
-
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
+                    $scope.select = function (day, $event) {
+                        tryStopProp($event);
                         if (day.disabled) {
                             return;
                         }
 
                         //both dates are already selected, reset dates
-                        var current = scope.getCurrentSelection();
+                        var current = $scope.getCurrentSelection();
 
                         var date = day.date;
                         if ((current.start && current.end) || !current.start) {
                             current.start = moment(date);
                             current.end = null;
-                            scope.inputDates[0] = current.start.format("YYYY-MM-DD");
+                            $scope.inputDates[0] = current.start.format(pickerDateFormat);
                         } else if (current.start && !current.end) {
                             if (current.start.isAfter(date, 'day')) {
                                 current.start = moment(date);
-                                scope.inputDates[0] = current.start.format("YYYY-MM-DD");
+                                $scope.inputDates[0] = current.start.format(pickerDateFormat);
                             }
                             else if (current.start.isBefore(date, 'day')) {
                                 current.end = moment(date);
-                                scope.inputDates[1] = current.end.format("YYYY-MM-DD");
+                                $scope.inputDates[1] = current.end.format(pickerDateFormat);
                             }
                         }
-                        scope.resetRangeClass();
+                        $scope.resetRangeClass();
                     };
 
-                    scope.setRange = function (range, $event) {
+                    $scope.setRange = function (range, $event) {
                         if (!range)
                             return;
                         if (range === CUSTOM) {
-                            scope.showCalendars = true;
+                            $scope.showCalendars = true;
                             return;
                         }
-                        scope.showCalendars = false;
-                        scope.currentSelection = range.clone();
-                        scope.selection = scope.currentSelection.clone();
-                        scope.ok($event);
+                        $scope.showCalendars = false;
+                        $scope.currentSelection = range.clone();
+                        $scope.selection = $scope.currentSelection.clone();
+                        $scope.ok($event);
                     };
 
-                    scope.ok = function ($event) {
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
-                        scope.model = scope.selection;
+                    $scope.ok = function ($event) {
+                        tryStopProp($event);
+                        $scope.model = $scope.selection;
                         $timeout(function () {
-                            if (scope.callback) {
-                                return scope.callback();
+                            if ($scope.callback) {
+                                return $scope.callback();
                             }
                         });
-                        return scope.hide();
+                        return $scope.hide();
                     };
 
-                    scope.clear = function ($event) {
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
-                        scope.selection = null;
-                        scope.ok($event);
+                    $scope.clear = function ($event) {
+                        $scope.selection = null;
+                        $scope.ok($event);
                     };
 
-                    scope.applySelection = function ($event) {
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
-                        scope.showCalendars = true;
-                        scope.selection = moment.range(scope.currentSelection.start.clone(), scope.currentSelection.end.clone());
-                        scope.ok($event);
+                    $scope.applySelection = function ($event) {
+                        $scope.showCalendars = true;
+                        $scope.selection = moment.range($scope.currentSelection.start.clone(), $scope.currentSelection.end.clone());
+                        $scope.ok($event);
                     }
 
-                    scope.move = function (date, n, $event) {
-                        if ($event !== null) {
-                            if (typeof $event.stopPropagation === "function") {
-                                $event.stopPropagation();
-                            }
-                        }
+                    $scope.move = function (date, n, $event) {
+                        tryStopProp($event);
 
                         var currentStart, currentEnd;
 
@@ -145,21 +129,21 @@
                             currentStart = currentEnd.clone().add(-1, 'months');
                         }
 
-                        scope.months[0] = createMonth(currentStart);
-                        scope.months[1] = createMonth(currentEnd);
+                        $scope.months[0] = createMonth(currentStart);
+                        $scope.months[1] = createMonth(currentEnd);
                     }
 
-                    scope.getCurrentSelection = function() {
-                        if (!scope.currentSelection && scope.selection)
-                            scope.currentSelection = scope.selection.clone();
-                        if (!scope.currentSelection)
-                            scope.currentSelection = {};
-                        return scope.currentSelection;
+                    $scope.getCurrentSelection = function() {
+                        if (!$scope.currentSelection && $scope.selection)
+                            $scope.currentSelection = $scope.selection.clone();
+                        if (!$scope.currentSelection)
+                            $scope.currentSelection = {};
+                        return $scope.currentSelection;
                     }
 
-                    scope.getClassName = function (day) {
+                    $scope.getClassName = function (day) {
 
-                        var current = scope.getCurrentSelection();
+                        var current = $scope.getCurrentSelection();
 
                         if (!day || day.number === false)
                             return "off";
@@ -175,11 +159,11 @@
                         return "available";
                     };
 
-                    scope.resetRangeClass = function () {
+                    $scope.resetRangeClass = function () {
                         var found = false;
-                        var current = scope.getCurrentSelection();
-                        for (var i = 0; i < scope.ranges.length; i++) {
-                            var item = scope.ranges[i];
+                        var current = $scope.getCurrentSelection();
+                        for (var i = 0; i < $scope.ranges.length; i++) {
+                            var item = $scope.ranges[i];
                             item.active = false;
                             if (item.range && item.range !== CUSTOM && current.start && current.end) {
                                 if (current.start.isSame(item.range.start, 'day') && current.end.isSame(item.range.end, 'day')) {
@@ -189,54 +173,54 @@
                             }
                         }
                         if (!found)
-                            scope.ranges[scope.ranges.length - 1].active = true;
+                            $scope.ranges[$scope.ranges.length - 1].active = true;
                     };
 
-                    scope.updateStartOrEndDate = function (first, last) {
-                        var current = scope.getCurrentSelection();
+                    $scope.updateStartOrEndDate = function (first, last) {
+                        var current = $scope.getCurrentSelection();
 
                         if (first) {
-                            var start = moment(scope.inputDates[0]);
+                            var start = moment($scope.inputDates[0]);
                             if (!start)
                                 return;
 
                             current.start = start;
                             if (!current.end || current.end.isBefore(start, 'day')) {
                                 current.end = start;
-                                scope.inputDates[1] = current.end.format("YYYY-MM-DD");
+                                $scope.inputDates[1] = current.end.format(pickerDateFormat);
                             }
                         } else if (last) {
-                            var end = moment(scope.inputDates[1]);
+                            var end = moment($scope.inputDates[1]);
                             if (!end)
                                 return;
 
                             current.end = end;
                             if (!current.start || current.start.isAfter(end, 'day')) {
                                 current.start = end;
-                                scope.inputDates[0] = current.start.format("YYYY-MM-DD");
+                                $scope.inputDates[0] = current.start.format(pickerDateFormat);
                             }
                         }
-                        scope.resetRangeClass();
+                        $scope.resetRangeClass();
                     }
 
-                    scope.moveToMonth = function (first, index) {
+                    $scope.moveToMonth = function (first, index) {
                         if (!first)
                             return;
 
-                        var start = moment(scope.inputDates[0]);
+                        var start = moment($scope.inputDates[0]);
                         if (!start)
                             return;
 
-                        if (!start.isSame(scope.months[index].date, 'month')) {
+                        if (!start.isSame($scope.months[index].date, 'month')) {
                             //move to month
-                            scope.months[0] = createMonth(start.clone());
-                            scope.months[1] = createMonth(start.clone().add(1, 'months'));
+                            $scope.months[0] = createMonth(start.clone());
+                            $scope.months[1] = createMonth(start.clone().add(1, 'months'));
                         }
                     }
 
                     /**************************************************************************************/
                     //load popup template
-                    var el = $compile(angular.element(getPickDateTemplate()))(scope);
+                    var el = $compile(angular.element(getPickDateTemplate()))($scope);
                     element.append(el);
 
                     element.bind("click", function (e) {
@@ -245,59 +229,59 @@
                                 e.stopPropagation();
                             }
                         }
-                        return scope.$apply(function () {
-                            if (scope.visible) {
-                                return scope.hide();
+                        return $scope.$apply(function () {
+                            if ($scope.visible) {
+                                return $scope.hide();
                             } else {
-                                return scope.show();
+                                return $scope.show();
                             }
                         });
                     });
                     var documentClickFn = function (e) {
-                        scope.$apply(function () {
-                            return scope.hide();
+                        $scope.$apply(function () {
+                            return $scope.hide();
                         });
                         return true;
                     };
                     angular.element(document).bind("click", documentClickFn);
-                    scope.$on('$destroy', function () {
+                    $scope.$on('$destroy', function () {
                         return angular.element(document).unbind('click', documentClickFn);
                     });
                 }
             };
 
-            function prepareRanges(scope) {
-                if (scope.ranges[scope.ranges.length - 1].range !== CUSTOM)
-                    scope.ranges.push({ label: 'Custom Range', range: CUSTOM });
+            function prepareRanges($scope) {
+                if ($scope.ranges[$scope.ranges.length - 1].range !== CUSTOM)
+                    $scope.ranges.push({ label: 'Custom Range', range: CUSTOM });
 
-                scope.resetRangeClass();
+                $scope.resetRangeClass();
 
-                if (scope.ranges[scope.ranges.length - 1].active)
-                    scope.showCalendars = true;
+                if ($scope.ranges[$scope.ranges.length - 1].active)
+                    $scope.showCalendars = true;
             };
 
-            function prepareMonths(scope) {
-                scope.months = [];
+            function prepareMonths($scope) {
+                $scope.months = [];
                 var start = null;
                 var end = null;
-                if (scope.model) {
-                    start = scope.model.start;
-                    end = scope.model.end;
+                if ($scope.model) {
+                    start = $scope.model.start;
+                    end = $scope.model.end;
                 }
 
                 if (!start) start = moment();
                 if (!end) end = moment();
 
-                scope.months.push(createMonth(start.clone().startOf("month")));
-                scope.months.push(createMonth(start.clone().startOf("month").add(1, "month")));
+                $scope.months.push(createMonth(start.clone().startOf("month")));
+                $scope.months.push(createMonth(start.clone().startOf("month").add(1, "month")));
 
-                scope.inputDates = [];
-                scope.inputDates.push(start.format("YYYY-MM-DD"));
-                scope.inputDates.push(end.format("YYYY-MM-DD"));
+                $scope.inputDates = [];
+                $scope.inputDates.push(start.format(pickerDateFormat));
+                $scope.inputDates.push(end.format(pickerDateFormat));
             }
 
             function createMonth(date) {
-                var month = { name: date.format("MMM YYYY"), date: date, weeks: getWeeks(date) };
+                var month = { name: date.format(monthFormat), date: date, weeks: getWeeks(date) };
                 return month;
             }
 
